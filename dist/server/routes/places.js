39e5999882bc3b4030a6d8fec8ce1b8caa17f21c
Rafael Ribeiro <rribeiro93k@gmail.com>
2026-02-07 15:22:18 -0300

    chore: remove dist folder

--- dist/server/routes/places.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const placesController_1 = require("../controllers/placesController");
-const router = (0, express_1.Router)();
-router.put('/categories/single', placesController_1.handleUpdateSinglePlaceCategory);
-exports.default = router;
